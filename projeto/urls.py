f935a3f32b487e9e16366d4da62d0dcca0dcd3b5
Wesley <wesley9063-700@outlook.com>
2026-08-05 20:01:20 -0300

    adiciona view personalizada e rota para 'sobre'

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# SOLICITATION -> HTTP REQUEST -> CLIENTE PEDE
+
+def my_view(request):
+    return HttpResponse('UMA LINDA STRING')
+    # RETURN -> HTTP RESPONSE -> SERVIDOR RESPONDE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
