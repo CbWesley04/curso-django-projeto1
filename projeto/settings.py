808c6208ad9542f8f4e86638608582d946a3deaf
Wesley <wesley9063-700@outlook.com>
2026-08-07 16:11:40 -0300

    adiciona templates e ajusta views para renderização correta da página inicial

--- projeto/settings.py
+++ projeto/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #APPS PADRÃO
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #MEUS APPS
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
