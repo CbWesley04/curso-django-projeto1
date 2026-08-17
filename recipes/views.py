01abd9a40d881fe2c42eb644746f576bd8b82831
Wesley <wesley9063-700@outlook.com>
2026-08-17 11:21:29 -0300

    Removendo contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
